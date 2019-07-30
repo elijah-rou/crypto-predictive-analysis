d7f8389a83a060612f3af784660dc9c06bf43ea6
Elijah Roussos <elijah.rou@gmail.com>
2019-07-30 17:42:27 +0200

    Script to query botometer for account score and store.

---
+++ src/twitter-sentiment/botScoreUpdater.py
@@ -0,0 +1,33 @@
+import botometer
+from pymongo import MongoClient
+import pprint
+
+# MongoDB connection
+client = MongoClient("192.168.2.69", 27017)
+db = client["sentiment_data"]
+users = db["twitter_users"]
+
+# Twitter Authentication
+twitterAuth = {
+    "consumer_key" : "LIrUnrnifiXrTaiuTmalM30Pd",
+    "consumer_secret": "iH9hlwQvrdzKhkwd1RzSiAhEUjnRltgYihR1n5YPO6FkOY81k9",
+    "access_token" : "1152134174108782592-qEFou1vhM61EI4tnUjzZRNsfeq8Enq",
+    "access_secret" : "89d5n5V0mviJeJyXdKirjKTLDKVAa6hbeKbUuM3SXX9Yq"
+}
+
+# Botometer API key
+botAPIKey = "cd3f38366emshdd2cd0a2a5cca9ep14172djsn5f6c4df5a085"
+bom = botometer.Botometer(wait_on_rate_limit = True, mashape_key = botAPIKey, **twitterAuth)
+
+# Grab 17000
+user_list = users.find({})
+user_list = list(user_list)
+
+# Grab botometer score for the first 17000 entries (first 10 done already)
+# & insert into Mongo
+for i in range(10, 17000):
+    result = bom.check_account(user_list[i]["id"])
+    result.pop("display_scores")
+    result.pop("user")
+    pprint.pprint(result)
+    users.update_one({"_id": user_list[i]["_id"]}, {"$set": result})
