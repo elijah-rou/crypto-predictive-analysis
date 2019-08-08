8dff6679cf3ea91a6ffb8f1b3c8582bc9e9746f7
Elijah Roussos <elijah.rou@gmail.com>
2019-08-08 09:09:34 +0200

    Created a script to plot correlation

---
+++ src/twitter-sentiment/correlationPlotter.py
@@ -0,0 +1,15 @@
+#%%
+import pandas as pd
+
+#%%
+pearson  =[0.02,0.07,-0.05,0.07,0.09,-0.01,-0.05,-0.08,-0.1]
+df = pd.DataFrame(\
+    {"Time Shift": [1,2,3,4,5,6,7,8,9],
+     "Pearson Coefficient": pearson,\
+     "p-Value": [0.71,0.26,0.46,0.31,0.16,0.87,0.43,0.21,0.14]})
+
+
+#%%
+ax = df.plot.bar(legend=False, figsize=(10,8),title="Pearson Correlation for various hourly time shifts on Twitter Sentiment vs. BTC price change", x="Time Shift", y="Pearson Coefficient", colormap="Paired")
+ax.set_ylabel("Pearson Coefficient")
+plt.savefig("output/pearson.png", dpi=300)
